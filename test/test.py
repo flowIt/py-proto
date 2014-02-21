f6226db0fa87f5dda82b715a178323389156f009
Christophe Courtaut <christophe.courtaut@gmail.com>
2014-02-21 17:11:17 +0100

    test: Moved test to unittest framework
    Signed-off-by: Christophe Courtaut <christophe.courtaut@gmail.com>

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import inspect
+import unittest
 
 # realpath() with make your script run, even if you symlink it :)
 cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
@@ -15,12 +16,25 @@ if cmd_subfolder not in sys.path:
 from Brick import Brick
 from Buffer import Buffer
 
-brick1 = Brick(0, 1)
-brick2 = Brick(1, 0)
-brick3 = Brick(1, 0)
-buf1 = Buffer()
-brick1.SetOut(0, buf1)
-brick2.SetIn(0, buf1)
-brick3.SetIn(0, buf1)
+class BrickConnectionTest(unittest.TestCase):
+    def setUp(self):
+        self.brick1 = Brick(0, 1)
+        self.brick2 = Brick(1, 0)
+        self.brick3 = Brick(1, 0)
+        self.buf = Buffer()
+    def testConnectToUnexistingBrickEntry(self):
+        self.assertRaises(Exception, self.brick1.SetIn, 0, self.buf)
+    def testConnectToAlreadyConnectedBrickEntry(self):
+        self.brick2.SetIn(0, self.buf)
+        self.assertRaises(Exception, self.brick2.SetIn, 0, self.buf)
+    def testConnectSomethingElseThanBuffer(self):
+        self.assertRaises(Exception, self.brick2.SetIn, 0, 0)
 
-brick1.DoCycle()
+class BrickExecutionTest(unittest.TestCase):
+    def setUp(self):
+        self.brick = Brick(1, 1)
+    def testDoCycle(self):
+        self.assertRaises(Exception, self.brick.DoCycle)
+
+if __name__ == '__main__':
+    unittest.main()
