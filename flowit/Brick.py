c315651025c27266b99281f706631b5c78cab03a
benjamin SCETBUN <Benny@Benny-PC.local>
2014-02-21 16:08:04 +0100

    Basic Brick and buffer with connection and error check ready

---
+++ flowit/Brick.py
@@ -0,0 +1,36 @@
+from Buffer import Buffer
+
+class Brick:
+	__idCount = 0
+	def __init__(self, nbIns, nbOuts):
+		self._id = Brick.__idCount
+		Brick.__idCount += 1
+		self.__nbIns = nbIns
+		self.__nbOuts = nbOuts
+		self._ins = {}
+		self._outs = {}
+	
+	def SetIn(self, inNumber, buffer):
+		if inNumber >= self.__nbIns or inNumber < 0:
+			raise Exception("Cannot set %d Input Buffer (Brick %d)" % (inNumber, self._id))
+		if inNumber in self._ins:
+			raise Exception("%d Output already connected (Brick %d)" % (inNumber, self._id))
+		if not isinstance(buffer, Buffer):
+			raise Exception("Cannot connect a non Buffer object (Brick %d)" % (self._id))
+		tmpId = buffer.setOut()
+		self._ins[inNumber] = [tmpId, buffer]
+		
+	
+	def SetOut(self, outNumber, buffer):
+		if outNumber >= self.__nbOuts or outNumber < 0:
+			raise Exception("Cannot set %d Output Buffer (Brick %d)" % (outNumber, self._id))
+		if outNumber in self._outs:
+			raise Exception("%d Output already connected (Brick %d)" % (outNumber, self._id))
+		if not isinstance(buffer, Buffer):
+			raise Exception("Cannot connect a non Buffer object (Brick %d)" % (self._id))
+		buffer.setIn(self._id)
+		self._outs[outNumber] = buffer
+		
+	def DoCycle(self):
+		raise Exception("This brick havent got DoCycle Defined!!! (Brick %d)" % (self._id))
+		return True
---
+++ flowit/Buffer.py
@@ -0,0 +1,39 @@
+class Buffer:
+	__idCount = 0
+	def __init__(self):
+		self._id = Buffer.__idCount
+		self.__connectionId = 0
+		Buffer.__idCount += 1
+		self._inId = -1
+		self._outCtxIds = []
+	
+	def __getConnectionId(self):
+		tmp = self.__connectionId
+		self.__connectionId += 1
+		return tmp
+	
+	def PushData(self, cur_id):
+		if cur_id is not self._inId:
+			raise Exception("Wrong Id(%d) in Buffer PushData (Buffer %d)" % (cur_id, self._id))
+			
+	def PopData(self, cur_id):
+		if cur_id not in self._outCtxIds:
+			raise Exception("This ConnectionId (%d) isnt connected to that Buffer (Buffer %d)" % (cur_id, self._id))
+	
+	def PeekData(self, cur_id):
+		if cur_id not in self._outCtxIds:
+			raise Exception("This ConnectionId (%d) isnt connected to that Buffer (Buffer %d)" % (cur_id, self._id))
+
+	def needData(self, cur_id):
+		if cur_id not in self._outCtxIds:
+			raise Exception("This ConnectionId (%d) isnt connected to that Buffer (Buffer %d)" % (cur_id, self._id))
+			
+	def setIn(self, cur_id):
+		if self._inId is not -1:
+			raise Exception("Cant connect %d as Input of Buffer, already connected (Buffer %d)" % (cur_id, self._id))
+		self._inId = cur_id
+
+	def setOut(self):
+		ret = self.__getConnectionId()
+		self._outCtxIds.append(ret)
+		return ret
